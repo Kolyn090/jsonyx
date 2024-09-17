50f643f3cc1aa7d88bfab743f0211d0d41f75eb1
Kolyn090 <kolynlin@protonmail.com>
2024-09-16 22:25:08 -0400

    Revert "Fixed missing list key due to lists all empty in table"

--- src/db_path.py
+++ src/db_path.py
@@ -1,6 +1,6 @@
 import secret_path
 
-data_dir = f'../usr/debug-db/data'
-system_dir = f'../usr/debug-db/system'
+data_dir = f'../usr/example-db/data'
+system_dir = f'../usr/example-db/system'
 # data_dir = secret_path.data_dir
 # system_dir = secret_path.system_dir
