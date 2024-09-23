35c8af0b6f851c019ca84166ab4f74e5e8d6c00f
Kolyn090 <jianxinlin@umass.edu>
2024-09-23 12:04:20 -0400

    Commit before further improving scope distribution

--- src/structure_checker.py
+++ src/structure_checker.py
@@ -19,21 +19,20 @@ def python_to_sql_type(py_type):
     return type_mapping.get(py_type, 'UNKNOWN')
 
 
-def get_node_type(node):
-    if isinstance(node, (dict, list)):
-        return 0 if isinstance(node, dict) else 1
-    return 2
+SQL_list = python_to_sql_type(list)
+SQL_dict = python_to_sql_type(dict)
+SQL_null = python_to_sql_type(None)
 
 
 class Node:
-    def __init__(self, key='', value_type=python_to_sql_type(None), parent=None, child_count=0):
+    def __init__(self, key='', value_type=SQL_null, parent=None, child_count=0):
         self.key = key
         self.value_type = value_type
         self.parent = parent
         self.child_count = child_count
 
     def __str__(self):
-        return f'key={self.key}, val_type={self.value_type}, child_count={self.child_count}'
+        return f'key={self.key}, val_type={self.value_type}, child_count={self.child_count}, isParentNone={self.parent is None}'
 
 
 def structure_check(json_dir, system_dir, data_dir):
@@ -44,6 +43,11 @@ def structure_check(json_dir, system_dir, data_dir):
             json_obj = json.loads(file.read())
 
     """ Search for scope """
+    def get_node_type_of(n):
+        if isinstance(n, (dict, list)):
+            return 0 if isinstance(n, dict) else 1
+        return 2
+
     # Keys must be from data / an empty string (if unnamed)
     # Names can be assigned by program
 
@@ -52,13 +56,12 @@ def structure_check(json_dir, system_dir, data_dir):
 
     # initialize name for dict under outermost list
     # List<(Node, Level)>
-    root = Node('ROOT', python_to_sql_type(list), None, len(json_obj))
-    pq = [(e, 0, root) for i, e in enumerate(json_obj)]
+    root = Node('ROOT', SQL_list, None, len(json_obj))
+    pq = [(e, 0, Node(parent=root)) for i, e in enumerate(json_obj)]
 
-    temp_name_idx = len(json_obj) - 1
     while len(pq) > 0:
         obj, node_level, curr_node = pq.pop(0)
-        obj_type = get_node_type(obj)
+        obj_type = get_node_type_of(obj)
 
         if len(scope) < node_level + 1:
             scope.append([])
@@ -72,9 +75,6 @@ def structure_check(json_dir, system_dir, data_dir):
         elif obj_type == 1:  # list
             curr_node.child_count = len(obj)
             for idx, item in enumerate(obj):
-                item_type = get_node_type(item)
-                if item_type == 0:  # item is a dict
-                    temp_name_idx += 1
                 new_node = Node('', python_to_sql_type(type(item)), curr_node)
                 pq.append((item, node_level + 1, new_node))
                 scope[node_level].append(new_node)
@@ -97,30 +97,92 @@ def structure_check(json_dir, system_dir, data_dir):
         for node in row:
             if node.key not in group:
                 group[node.key] = []
+            # TODO
+            # Same parent cannot be in the same scope
             group[node.key].append(node)
         if len(group) > 0:
             fake_same_scope.append(group)
-    # for idx, group in enumerate(fake_same_scope):
-    #     print(f'{idx} -------------------')
-    #     for k, nodes in group.items():
-    #         print(k)
-    #         for node in nodes:
-    #             print(node)
-    # print()
-
+    for idx, group in enumerate(fake_same_scope):
+        print(f'{idx} -------------------')
+        for k, nodes in group.items():
+            print(k)
+            for node in nodes:
+                print(node)
+    print()
+'''
     """ Determine validness of structure """
+    def find_closest_list_ancestor_of(n):
+        temp = n.parent
+        if temp is None:
+            return n
+        prev = temp
+        while temp.value_type != SQL_list:
+            temp = temp.parent
+            if temp is None:
+                return prev
+            prev = temp
+        return temp
+
     for group in fake_same_scope:
+
         for nodes in group.values():
+            if len(nodes) <= 1:
+                continue
+
             # TODO:
             # Compare e1, e2:
             # 1. If parent is a Dict, its parent must be different from the rest of nodes in scope
-            # 2. Must match siblings count = closest ancestor list child count (need to carefully handle 'fake')
+            # 2. Must match siblings count = list scope count * closest ancestor list child count
+            #    Scope siblings count = list ancestor count of list ancestor
+            #    (need to carefully handle 'fake')
             # 3. Parent of list cannot be list
 
-            pass
+            # Rule 1
+            for idx1 in range(len(nodes)-1):
+                e1 = nodes[idx1]
+                e1_parent = e1.parent
+                is_parent_dict = e1_parent.value_type == SQL_dict
+                for idx2 in range(idx1+1, len(nodes)):
+                    e2 = nodes[idx2]
+                    e2_parent = e2.parent
+                    if is_parent_dict:
+                        if e1_parent is e2_parent:
+                            print('Violation of rule 1')
+                            return False
+            # Rule 2
+            for idx1 in range(len(nodes)):
+                e1 = nodes[idx1]
+                e1_closest_list_ancestor = find_closest_list_ancestor_of(e1)
+                if e1_closest_list_ancestor is None:  # The Root
+                    continue
+                e1_common_ancestor = find_closest_list_ancestor_of(e1_closest_list_ancestor)
+                scope_siblings_count = e1_common_ancestor.child_count
+                print(e1)
+                print(e1_closest_list_ancestor)
+                print(e1_common_ancestor)
+                print(scope_siblings_count)
+                counter = 0
+                for idx2 in range(len(nodes)):
+                    e2 = nodes[idx2]
+                    e2_closest_list_ancestor = find_closest_list_ancestor_of(e2)
+                    if e1_closest_list_ancestor is e2_closest_list_ancestor:
+                        counter += 1
+                if counter != scope_siblings_count:
+                    print(counter)
+                    print('Violation of rule 2')
+                    return False
+            # Rule 3
+            for node in nodes:
+                if node.value_type == SQL_list:
+                    if node.parent is not None:
+                        if node.parent.value_type == SQL_list:
+                            print('Violation of rule 3')
+                            return False
+    return True
+'''
 
 
 if __name__ == "__main__":
     jsons = util.get_jsons_under(db_path.data_dir)
     for j in jsons:
-        structure_check(j, db_path.system_dir, db_path.data_dir)
+        print(structure_check(j, db_path.system_dir, db_path.data_dir))
