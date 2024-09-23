94ac958c34a35e7c35ca48b6096645d745b28e23
Kolyn090 <jianxinlin@umass.edu>
2024-09-23 00:55:18 -0400

    Rewrote traverse algorithm: keep reference to parent

--- src/structure_checker.py
+++ src/structure_checker.py
@@ -26,10 +26,14 @@ def get_node_type(node):
 
 
 class Node:
-    def __init__(self):
-        self.key = ''
-        self.value_type = python_to_sql_type(None)
-        self.parent = None
+    def __init__(self, key='', value_type=python_to_sql_type(None), parent=None, child_count=0):
+        self.key = key
+        self.value_type = value_type
+        self.parent = parent
+        self.child_count = child_count
+
+    def __str__(self):
+        return f'key={self.key}, val_type={self.value_type}, child_count={self.child_count}'
 
 
 def structure_check(json_dir, system_dir, data_dir):
@@ -47,77 +51,73 @@ def structure_check(json_dir, system_dir, data_dir):
     scope = []
 
     # initialize name for dict under outermost list
-    # List<(Node, Level, Node_Name)>
-    pq = [(e, 0, f'n{i}') for i, e in enumerate(json_obj)]
-    first_row = [(tu[2], python_to_sql_type(dict), 'ROOT', 1) for tu in pq]
+    # List<(Node, Level)>
+    root = Node('ROOT', python_to_sql_type(list), None, len(json_obj))
+    pq = [(e, 0, root) for i, e in enumerate(json_obj)]
 
     temp_name_idx = len(json_obj) - 1
     while len(pq) > 0:
-        curr_node, node_level, node_name = pq.pop(0)
-        curr_type = get_node_type(curr_node)
+        obj, node_level, curr_node = pq.pop(0)
+        obj_type = get_node_type(obj)
 
         if len(scope) < node_level + 1:
             scope.append([])
 
-        if curr_type == 0:  # dict
-            for k, v in curr_node.items():
-                pq.append((v, node_level + 1, k))
-                scope[node_level].append((k, python_to_sql_type(type(v)), node_name, curr_type))
-        elif curr_type == 1:  # list
-            for idx, item in enumerate(curr_node):
+        if obj_type == 0:  # dict
+            curr_node.child_count = len(obj.items())
+            for k, v in obj.items():
+                new_node = Node(k, python_to_sql_type(type(v)), curr_node)
+                pq.append((v, node_level + 1, new_node))
+                scope[node_level].append(new_node)
+        elif obj_type == 1:  # list
+            curr_node.child_count = len(obj)
+            for idx, item in enumerate(obj):
                 item_type = get_node_type(item)
                 if item_type == 0:  # item is a dict
                     temp_name_idx += 1
+                new_node = Node('', python_to_sql_type(type(item)), curr_node)
+                pq.append((item, node_level + 1, new_node))
+                scope[node_level].append(new_node)
 
-                pq.append((item, node_level + 1, f'n{temp_name_idx}'))
-                scope[node_level].append((f'n{temp_name_idx}', python_to_sql_type(type(item)), node_name, curr_type))
-
-    scope.insert(0, first_row)  # outermost
-    # print(scope)
+    scope.insert(0, [root])
+    scope.pop(-1)
+    # for idx, row in enumerate(scope):
+    #     print(f'{idx} -------------------')
+    #     for node in row:
+    #         print(node)
 
     """ Group records to the same scope """
-    same_scope = []
-    for level in scope:
+    # It's fake because it doesn't handle the case of keys
+    # in list with keys in dict located at the same level.
+    fake_same_scope = []
+    for row in scope:
         group = {}
         # Notice that parent type is 0, 1, 2
         # while type is SQL type
-        for key, tp, parent_key, parent_tp in level:
-            if (parent_key, parent_tp) not in group:
-                group[(parent_key, parent_tp)] = []
-            group[(parent_key, parent_tp)].append((key, tp))
+        for node in row:
+            if node.key not in group:
+                group[node.key] = []
+            group[node.key].append(node)
         if len(group) > 0:
-            same_scope.append(group)
+            fake_same_scope.append(group)
+    # for idx, group in enumerate(fake_same_scope):
+    #     print(f'{idx} -------------------')
+    #     for k, nodes in group.items():
+    #         print(k)
+    #         for node in nodes:
+    #             print(node)
+    # print()
 
-    for line in same_scope:
-        print(line)
-    print()
     """ Determine validness of structure """
+    for group in fake_same_scope:
+        for nodes in group.values():
+            # TODO:
+            # Compare e1, e2:
+            # 1. If parent is a Dict, its parent must be different from the rest of nodes in scope
+            # 2. Must match siblings count = closest ancestor list child count (need to carefully handle 'fake')
+            # 3. Parent of list cannot be list
 
-
-    # for field_dict in same_scope:
-    #     field_dict_vals = field_dict.values()
-    #     # 0 (dict), 1 (list), 2 (val) type
-    #     print(list(field_dict_vals))
-    #     instance_parent_type = list(field_dict_vals)[0][2]
-    #
-    #     # type, parent_name, parent_type (0, 1, 2)
-    #     # ex. instance_list >> [('VARCHAR', '', 0), ('INT', '', 0)]
-    #     for instance_list in field_dict_vals:
-    #         # To be in the same scope:
-    #         # If parent is Dict, parent_name must be different
-    #         # except for unnamed parent: ''.
-    #         # If parent is List, parent_name must be same.
-    #
-    #         if instance_parent_type == 0:  # dict
-    #             if instance_list[1] == '':  # unnamed dict
-    #                 pass
-    #             else:  # named dict
-    #                 pass
-    #         else:  # list
-    #             if instance_list[1] == '':  # unnamed list
-    #                 pass
-    #             else:  # named list
-    #                 pass
+            pass
 
 
 if __name__ == "__main__":
